3233e084135b96f670e7c100820e4fc2feff4f68
Eugene Obukhov <eugene.dos@gmail.com>
2012-01-24 13:42:03 +0400

    Deploy at 2012-01-24 13:42:03 +0400

--- app/admin/records.rb
+++ app/admin/records.rb
@@ -2,6 +2,20 @@
 ActiveAdmin.register Record do
   menu :label => "Записи разговоров"
   
+  filter :client, :label => "По Клиенту"
+  filter :called,  :label => "По исходящему номеру телефона", :as => :select, :collection =>  Phone.where('record is true')
+  filter :calling,  :label => "По входящему номеру телефона", :as => :select, :collection =>  Phone.where('record is true')
   
+  index do
+    column "Клиент", :client
+    column "Направление", :name
+    column "Инициатор вызова", :called
+    column "Назначение вызова", :calling
+    column "Детализация" do |record|
+      link_to "Скачать", record.link
+    end
+    
+  end
+
   
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,5 @@
 class Record < ActiveRecord::Base
+  belongs_to :phone
+  belongs_to :client
+  
 end
